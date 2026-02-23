05e0028e993e0ab58bb03b00af9d1b04770ca03c
lengkur <guo374655@gmail.com>
2026-02-23 20:35:52 +0800

    成功发送ipv4包（protocol17）

--- apps/ip_raw.cc
+++ apps/ip_raw.cc
@@ -9,6 +9,7 @@ enum class ProtocolType : uint8_t
   RAW = 5,
   UDP = 17
 };
+
 class IPv4
 {
 public:
@@ -16,35 +17,59 @@ public:
   uint8_t ver_ = 4;
   uint8_t tos_ {};
   uint16_t total_length_ {};
-  uint16_t identification_ {};
+  uint16_t identification_ = 0xabcd;
   uint16_t fram_off_ {};
   uint8_t flag_ {};
   uint8_t ttl_ = 64;
   uint8_t protocol_ {};
-  uint16_t checksum_ {};
-  std::array<uint8_t, 4> source_;
-  std::array<uint8_t, 4> destination_;
+  std::array<uint8_t, 4> source_ {};
+  std::array<uint8_t, 4> destination_ {};
+  uint16_t checksum_ = 0;
+  std::string data_ {};
 
-  std::string to_packet();
-  explicit IPv4( std::size_t PayloadLength,
+  explicit IPv4( const std::string& payload,
                  std::array<uint8_t, 4> source = { 127, 0, 0, 1 },
                  uint8_t Protocol = 5,
                  std::array<uint8_t, 4> destination = { 127, 0, 0, 1 } )
-    : total_length_( 20 + PayloadLength ), protocol_( Protocol ), source_( source ), destination_( destination )
+    : total_length_( 20 + payload.size() + ( static_cast<ProtocolType>( Protocol ) == ProtocolType::UDP ? 8 : 0 ) )
+    , protocol_( Protocol )
+    , source_( source )
+    , destination_( destination )
+    , data_( payload )
   {
-    std::string packet = to_packet();
-    std::uint32_t sum = 0;
-    for ( std::size_t i = 0; i < 20; i += 2 ) {
-      sum += ( packet.at( i ) << 0x8 ) | packet.at( i + 1 );
+    if ( data_.size() % 2 == 1 ) {
+      data_.push_back( '\0' );
     }
-    while ( sum >> 0x10 ) {
-      sum = ( sum & 0xffff ) + ( sum >> 0x10 );
-    }
-    checksum_ = static_cast<uint16_t>( ~sum );
+    checksum_ = compute_checksum_();
   }
-};
 
-std::string IPv4::to_packet()
+  std::string to_packet();
+  std::uint16_t compute_checksum_();
+};
+class UDP : public IPv4
+{
+public:
+  uint16_t source_port_ = 80;
+  uint16_t destination_port_ = 80;
+  uint16_t udp_length_ {};
+  uint16_t udp_checksum_ {};
+
+  explicit UDP( const std::string& payload,
+                std::array<uint8_t, 4> source = { 127, 0, 0, 1 },
+                uint16_t source_port = 80,
+                std::array<uint8_t, 4> destination = { 127, 0, 0, 1 },
+                uint16_t destination_port = 80 )
+    : IPv4( payload, source, 17, destination )
+    , source_port_( source_port )
+    , destination_port_( destination_port )
+    , udp_length_( static_cast<uint16_t>( 8 + payload.size() ) )
+    , udp_checksum_( compute_udp_checksum_() )
+  {}
+
+  std::string to_udp_packet();
+  std::uint16_t compute_udp_checksum_();
+};
+std::string IPv4::to_packet() // 把数据转成标准ip包流格式
 {
   std::string packet( 20, '\0' );
   packet[0] = static_cast<char>( ( ver_ << 4 ) | ihl_ );
@@ -54,7 +79,7 @@ std::string IPv4::to_packet()
   packet[4] = static_cast<char>( identification_ >> 8 );
   packet[5] = static_cast<char>( identification_ & 0x00ff );
 
-  packet[6] = static_cast<char>( flag_ << 5 | ( this->fram_off_ >> 8 ) );
+  packet[6] = static_cast<char>( flag_ << 5 | ( fram_off_ >> 8 ) );
   packet[7] = static_cast<char>( fram_off_ & 0x00ff );
 
   packet[8] = static_cast<char>( ttl_ );
@@ -74,9 +99,62 @@ std::string IPv4::to_packet()
   packet[19] = static_cast<char>( destination_[3] );
   return packet;
 }
+std::uint16_t IPv4::compute_checksum_() // 计算ip头checksum值
+{
+  std::string packet = to_packet();
+  std::uint32_t sum = 0;
+  packet[10] = packet[11] = 0;
+  for ( std::size_t i = 0; i < 20; i += 2 ) {
+    sum += ( static_cast<uint8_t>( packet.at( i ) ) << 0x8 ) | packet.at( i + 1 );
+  }
+  while ( sum >> 0x10 ) {
+    sum = ( sum & 0xffff ) + ( sum >> 0x10 );
+  }
+  return checksum_ = static_cast<uint16_t>( ~sum );
+}
+
+std::string UDP::to_udp_packet() // 把数据转成标准udp包流格式
+{
+  std::string udp_packet = to_packet();
+  udp_packet.resize( udp_packet.size() + 8 );
+  udp_packet[20] = static_cast<char>( source_port_ >> 8 );
+  udp_packet[21] = static_cast<char>( source_port_ & 0x00ff );
+  udp_packet[22] = static_cast<char>( destination_port_ >> 8 );
+  udp_packet[23] = static_cast<char>( destination_port_ & 0x00ff );
+  udp_packet[24] = static_cast<char>( udp_length_ >> 8 );
+  udp_packet[25] = static_cast<char>( udp_length_ & 0x00ff );
+  udp_packet[26] = static_cast<char>( udp_checksum_ >> 8 );
+  udp_packet[27] = static_cast<char>( udp_checksum_ & 0x00ff );
+
+  return udp_packet + data_;
+}
+
+std::uint16_t UDP::compute_udp_checksum_() // 计算udp头的checksum值
+{
+  std::uint32_t sum = 0;
+  std::uint32_t pseudo_sum = 0;
+  pseudo_sum += static_cast<uint8_t>( source_[0] ) << 8 | source_[1];
+  pseudo_sum += static_cast<uint8_t>( source_[2] ) << 8 | source_[3];
+  pseudo_sum += static_cast<uint8_t>( destination_[0] ) << 8 | destination_[1];
+  pseudo_sum += static_cast<uint8_t>( destination_[2] ) << 8 | destination_[3];
+  pseudo_sum += ( 0 << 8 ) | protocol_;
+  pseudo_sum += udp_length_;
+
+  sum += pseudo_sum;
+  sum += source_port_;
+  sum += destination_port_;
+  sum += udp_length_;
+  for ( size_t i = 0; i < data_.size(); i += 2 ) {
+    sum += ( static_cast<uint8_t>( data_.at( i ) ) << 0x8 ) | data_.at( i + 1 );
+  }
+  while ( sum >> 0x10 ) {
+    sum = ( sum & 0xffff ) + ( sum >> 0x10 );
+  }
+  return udp_checksum_ = static_cast<uint16_t>( ~sum );
+}
 
 namespace {
-std::array<uint8_t, 4> ip_to_array( const std::string& target )
+std::array<uint8_t, 4> ip_to_array( const std::string& target ) // 把ipv4地址四部分转成数组
 {
   std::array<uint8_t, 4> result {};
   uint8_t sum = 0;
@@ -93,18 +171,20 @@ std::array<uint8_t, 4> ip_to_array( const std::string& target )
   return result;
 }
 } // namespace
+
 int main()
 {
   // construct an Internet or user datagram here, and send using a RawSocket
   RawSocket server;
   std::string data = "Hello,I'm Lengkur.'";
-  std::string source_ip = server.local_address().ip();
+  std::string source_ip = server.local_address().ip(); // 获取本机地址
   std::string destination_ip = "127.0.0.1";
-  IPv4 payload( data.size(),
-                ip_to_array( source_ip ),
-                static_cast<uint8_t>( ProtocolType::RAW ),
-                ip_to_array( destination_ip ) );
+  IPv4 payload(
+    data, ip_to_array( source_ip ), static_cast<uint8_t>( ProtocolType::RAW ), ip_to_array( destination_ip ) );
 
-  server.send( payload.to_packet() + data, Address( destination_ip ) );
+  server.send( payload.to_packet() + payload.data_, Address( destination_ip ) ); // 测试protocol5的ip包
+
+  UDP payload_udp( data, ip_to_array( source_ip ), 80, ip_to_array( destination_ip ), 80 );
+  server.send( payload_udp.to_udp_packet(), Address( destination_ip ) ); // 测试protocol17的ip包
   return 0;
 }
